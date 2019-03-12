d07520745a27eafdb144033800b47af76165eb24
Colin Tracy <colin@aiselng.com>
2019-03-12 00:45:53 -0600

    add environment vars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
+
 module.exports = 
   {
+    
     webpack: config => {
       // Fixes npm packages that depend on `fs` module
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv)),
       config.node = {
         fs: "empty"
       }
