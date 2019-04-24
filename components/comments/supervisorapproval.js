eb3fc1e2f9a217fbe33c3cad36844bc0b45c4123
Colin Tracy <colin@aiselng.com>
2019-04-24 10:04:07 -0600

    super approval edits with notifications in tow

--- components/comments/supervisorapproval.js
+++ components/comments/supervisorapproval.js
@@ -1,6 +1,6 @@
 import gql from "graphql-tag"
 import { Box, Form, Select, Button } from "grommet"
-import { Mutation } from "react-apollo"
+import { Query, Mutation } from "react-apollo"
 
 import Authorization from "../../lib/auth/msal-auth"
 import { emailNotification } from "../../lib/auth/msal-graph"
@@ -35,6 +35,18 @@ const UPDATE_SUPERVISOR_APPROVAL = gql`
         }
     }
 `
+
+const GET_ADMIN_USERS = gql`
+    query fetchAdminUsers{
+        admins: fetchAdminUsers{
+            id
+            name
+            email
+            secondaryEmail
+        }
+    }
+`
+
 const emailNotifications = async (message) => {
     const auth = new Authorization()
 
@@ -48,74 +60,96 @@ const emailNotifications = async (message) => {
     }
 }
 
+const message = (admins, user) => {
+    const adminEmails = []
+    
+    for (let admin of admins) {
+        adminEmails.push(admin.email)
+    }
+    adminEmails.push(user)
+    let emailAddresses = Array.from(new Set(adminEmails));
+    
+    return emailAddresses
+}
+
 const SupervisorApproval = (props) => {
     const [value, setValue] = React.useState("")
     const currentProgress = value.id === 2 ? 9 : 3
     const currentReward = value.id === 2 ? 2 : null
-    const userMessage = supervisorReviewNotification(props.user, props.submissionId)
-
+    
     return (
-        <Mutation 
-            mutation={UPDATE_SUPERVISOR_APPROVAL}
-            onCompleted={
-                () => {
-                    emailNotifications(userMessage)
-                }
-            }
-        >
-            {(updateSupervisorApproval, {data}) => (
-                <Box    
-                    width="auto"
-                    flex={true}
-                    fill={false}
-                    align="end"
-                    pad={{ top: "3px", horizontal: "18px" }}
+        <Query query={GET_ADMIN_USERS}>
+        {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error :(</p>
+            const emails = message(data.admins, props.user)
+            
+            return (
+                <Mutation 
+                    mutation={UPDATE_SUPERVISOR_APPROVAL}
+                    onCompleted={
+                        () => {
+                            const userMessage = supervisorReviewNotification(emails, props.submissionId)
+                            emailNotifications(userMessage)
+                        }
+                    }
                 >
-                    <Form
-                        onSubmit={e => {
-                            e.preventDefault();
-                            updateSupervisorApproval({ variables: { 
-                                id: props.submissionId,
-                                progress: currentProgress,
-                                supervisorapproval: value.id,
-                                reward: currentReward
-                            } });
-                        }}
-                    >
-                        <Select 
-                            labelKey="name"
-                            valueKey="id"
-                            options={props.status}
-                            className="suggestionDropDown"
-                            value={value}
-                            placeholder="Endorse or Reject"
-                            alignSelf="end"
-                            size="small"
-                            plain={true}
-                            onChange={({ option }) => setValue(option)}
-                        />
-                        <Button type="submit" className="updateSubmissionButton" label="Submit" />
-                    </Form>
-                    <style jsx global>{`
-                        button.updateSubmissionButton {
-                            border: none;
-                            font-size: 15px;
-                            color: white;
-                            background: #D0011B;
-                            border-radius: 4px;
-                            margin-left: 15px;
-                            transition: background 0.3s ease-in-out;
-                            will-change: background;
-                        }
-                        button.updateSubmissionButton:hover {
-                            border: none;
-                            box-shadow: none;
-                            background: black;
-                        }
-                    `}</style>
-                </Box>
-            )}
-        </Mutation>
+                    {(updateSupervisorApproval, {data}) => (
+                        <Box    
+                            width="auto"
+                            flex={true}
+                            fill={false}
+                            align="end"
+                            pad={{ top: "3px", horizontal: "18px" }}
+                        >
+                            <Form
+                                onSubmit={e => {
+                                    e.preventDefault();
+                                    updateSupervisorApproval({ variables: { 
+                                        id: props.submissionId,
+                                        progress: currentProgress,
+                                        supervisorapproval: value.id,
+                                        reward: currentReward
+                                    } });
+                                }}
+                            >
+                                <Select 
+                                    labelKey="name"
+                                    valueKey="id"
+                                    options={props.status}
+                                    className="suggestionDropDown"
+                                    value={value}
+                                    placeholder="Endorse or Reject"
+                                    alignSelf="end"
+                                    size="small"
+                                    plain={true}
+                                    onChange={({ option }) => setValue(option)}
+                                />
+                                <Button type="submit" className="updateSubmissionButton" label="Submit" />
+                            </Form>
+                            <style jsx global>{`
+                                button.updateSubmissionButton {
+                                    border: none;
+                                    font-size: 15px;
+                                    color: white;
+                                    background: #D0011B;
+                                    border-radius: 4px;
+                                    margin-left: 15px;
+                                    transition: background 0.3s ease-in-out;
+                                    will-change: background;
+                                }
+                                button.updateSubmissionButton:hover {
+                                    border: none;
+                                    box-shadow: none;
+                                    background: black;
+                                }
+                            `}</style>
+                        </Box>
+                    )}
+                </Mutation>
+            )
+        }}
+        </Query>
     )
 }
 
