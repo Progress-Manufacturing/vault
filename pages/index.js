55fb9bf53a5c7300102d44d6cddb1fb9aa46165e
Colin Tracy <colin@aiselng.com>
2019-05-15 09:26:46 -0600

    update home br

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,10 @@ class Home extends Component {
           <Main isSupervisor={isSupervisor} isLead={isLead} isAdmin={isAdmin}>          
             <Card title={`Welcome, ${userFirstName}`} highlight={true}>
               <p style={{ fontSize: "14px" }}>
-				Welcome to The Vault! <br><br>
-				The Vault is Progress MFG’s portal to access different applications throughout the company. Do you need to put in a suggestion for continual improvement or safety? Did you just catch a co-worker living the company values and need to submit an iPoint? Do you need to submit a request for time off, or IT helpdesk, or maintenance? If you need to do any of these things, you have come to the right place!<br><br>
-				The Vault also has links to important websites and access to company tools.  <br><br>
+                Welcome to The Vault! <br /><br />
+                The Vault is Progress MFG’s portal to access different applications throughout the company. Do you need to put in a suggestion for continual improvement or safety? Did you just catch a co-worker living the company values and need to submit an iPoint? Do you need to submit a request for time off, or IT helpdesk, or maintenance? If you need to do any of these things, you have come to the right place!<br/><br/>
+				        The Vault also has links to important websites and access to company tools.  <br /><br /><br />
               </p>
-
             </Card>
           </Main>
         )}
