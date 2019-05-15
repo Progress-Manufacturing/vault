177feee21fa58afe9b71f9de03e177cb573b4777
Colin Tracy <colin@aiselng.com>
2019-05-15 09:08:22 -0600

    complete refactor of login msal-auth to accomidate for no loginRedirect

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,10 @@
-import React, { Component } from "react"
-import Main from "../lib/layout/login"
-import LoginButton from "../components/login/loginbutton"
+import Main from '../lib/layout/login';
+import LoginButton from '../components/login/loginbutton';
 
-class Login extends Component {
-
-  render() {
-    return (
-      <Main>
-        <LoginButton/>
-      </Main>
-    )
-  }
-}
+const Login = () => (  
+  <Main>
+    <LoginButton />
+  </Main>
+)
 
 export default Login
